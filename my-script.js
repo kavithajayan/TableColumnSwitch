05f6a57e40a7a1d378a9046bf533cc5cb5aca5ba
kavitha <kavitha@qburst.com>
2017-06-07 13:53:30 +0530

    added files

---
+++ my-script.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function (){
+	$('.choice').hide();
+	$('.one').show();
+	$('#sel').on('change', function () {
+	
+  	var val = $(this).val();
+    target = '.' + val;
+	$('.choice').hide();
+	$(target).show();
+	});
+});
