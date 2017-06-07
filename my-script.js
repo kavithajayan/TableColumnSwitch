15245ec8861cea1acab486afb976cba938157276
kavitha <kavitha@qburst.com>
2017-06-07 15:44:18 +0530

    corrected for desktop table

--- my-script.js
+++ my-script.js
@@ -1,13 +1,22 @@
-
-
-$(document).ready(function (){
-	$('.choice').hide();
-	$('.one').show();
-	$('#sel').on('change', function () {
-	
-  	var val = $(this).val(),
-    target = '.' + val;
-	$('.choice').hide();
-	$(target).show();
-	});
+$(document).ready(function() {
+    columnSwitch ();
 });
+
+$(window).resize(function() {
+	columnSwitch ();
+});
+
+function columnSwitch() {
+	if ($(window).width() < 767) {
+	    $('.choice').hide();
+	    $('.one').show();
+	    $('#sel').on('change', function() {
+	        var val = $(this).val(),
+	            target = '.' + val;
+	        $('.choice').hide();
+	        $(target).show();
+	    });
+	} else {
+		$('.choice').show();
+	}
+}
